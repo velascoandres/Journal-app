3f1d02526eb455fdf6cf0864b56a55f9492dcb7f
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-25 23:17:38 -0500

    estilos loginscreen

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginScreen: React.FC = () => {
-    return (
-        <div>
-            <h1>Login Screen</h1>
-        </div>
-    );
+	return (
+		<>
+			<h3 className="auth__title" >Login Screen</h3>
+			<form action="">
+				<input
+					type="text"
+					placeholder="email"
+					className="auth__input"
+					name="email"
+					autoComplete="off"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					className="auth__input"
+					name="password"
+				/>
+
+				<button
+					className="btn btn-primary btn-block"
+					type="submit"
+				>
+					Login
+				</button>
+
+				<hr />
+
+				<div className="auth__social-networks">
+					<p>Login with social networks</p>
+
+					<div className="google-btn">
+						<div className="google-icon-wrapper">
+							<img
+								className="google-icon"
+								src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+								alt="google button"
+							/>
+						</div>
+						<p className="btn-text">
+							<b>Sign in with google</b>
+						</p>
+
+					</div>
+				</div>
+
+
+				<Link
+					to="/auth/register"
+				>
+					Create new account
+				</Link>
+			</form>
+		</>
+	);
 };
--- src/routers/AuthRouter.tsx
+++ src/routers/AuthRouter.tsx
@@ -5,21 +5,27 @@ import {RegisterScreen} from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
     return (
-            <Switch>
-                <Route
-                    exact
-                    path="/auth/login"
-                    component={LoginScreen}
-                />
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route
+                        exact
+                        path="/auth/login"
+                        component={LoginScreen}
+                    />
 
-                <Route
-                    exact
-                    path="/auth/register"
-                    component={RegisterScreen}
-                />
+                     <Route
+                        exact
+                        path="/auth/register"
+                        component={RegisterScreen}
+                    />
 
-                <Redirect to="/auth/login"/>
-            </Switch>
+                   <Redirect to="/auth/login"/>
+                </Switch>
+            </div>
+            
+        </div>
+
     );
 };
 
