a891aa84695ffde0fcb8220b29ce4ae83f710191
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-27 15:31:45 -0500

    register form

--- src/components/auth/RegisterScreen.tsx
+++ src/components/auth/RegisterScreen.tsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/userForm';
+
+type RegisterFormValues = {
+    name: string;
+    email: string;
+    password: string;
+    password2: string;
+};
+
+
+const initialRegisterForm: RegisterFormValues = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+};
+
 
 export const RegisterScreen: React.FC = () => {
+    
+    const [formValues, handleInput] = useForm<RegisterFormValues>(initialRegisterForm)
+    
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(formValues);
+    };
+
+    const validateForm = () => {
+        // TODO: implement
+    };
+    
     return (
         <>
             <h3 className="auth__title" >Register Screen</h3>
-            <form>
+            <form
+                onSubmit={handleSubmit}
+            >
                 <input
                     type="text"
                     placeholder="Name"
                     className="auth__input"
                     name="name"
+                    onChange={handleInput}
                     autoComplete="off"
                 />
                 <input
                     type="text"
                     placeholder="Email"
                     className="auth__input"
+                    onChange={handleInput}
                     name="email"
                     autoComplete="off"
                 />
@@ -24,12 +57,14 @@ export const RegisterScreen: React.FC = () => {
                     type="password"
                     placeholder="Password"
                     className="auth__input"
+                    onChange={handleInput}
                     name="password"
                 />
                 <input
                     type="password"
                     placeholder="Confirm password"
                     className="auth__input"
+                    onChange={handleInput}
                     name="password2"
                 />
 
